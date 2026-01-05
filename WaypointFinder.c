c51577784f8d96019faee5a5c8cdf9ec4c754c7d
dan <dlascala@protonmail.com>
2026-01-05 12:50:30 -0800

    head points to beginning of points

--- WaypointFinder.c
+++ WaypointFinder.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define NUM_POINTS 5
 
 typedef struct {
     float x, y;
-    struct Waypoint *next;
 } Waypoint;
+
+typedef struct {
+    Waypoint point;
+    struct Node *next;
+} Node;
 
 Waypoint path[NUM_POINTS] = {
     {0, 0},
@@ -13,8 +18,14 @@ Waypoint path[NUM_POINTS] = {
     {1, 1},
     {1, 2},
     {2, 2},
-}
+};
+
 int main()
 {
-
+    Node *head = NULL;
+    head = malloc(sizeof(Node));
+    head->point = path[0];
+    head->next = NULL;
+//Test to confirm head points to the right place
+    printf("x: %.1f, y: %.1f\n", head->point.x, head->point.y);
 }
